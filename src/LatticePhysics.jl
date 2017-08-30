6382ceb25188c5ecb6c2d2948d4e907a30ac189f
Jan Attig <jattig@thp.Uni-Koeln.DE>
2017-08-30 14:04:08 +0200

    added unitcell building grom sites in 2 dimensions

--- src/LatticePhysics.jl
+++ src/LatticePhysics.jl
@@ -2243,8 +2243,147 @@ export getUnitcell_10_3_c
 # getUnitcellFromSites(basis, lattice_vectors)
 #
 
-
-
+function getUnitcellFromSites2D(
+        sites,
+        lattice_vectors;
+        max_ij=3,
+        epsilon=1e-8,
+        min_NN=-1,
+        max_NN=-1,
+        strength_NN=1.0,
+        name::String="AUTO",
+        save=false
+    )
+    # make a new list with connections
+    connections = Array[]
+    # define what "close to" means
+    function distance(p1, p2)
+        return sqrt(sum((p1.-p2).*(p1.-p2)))
+    end
+    function closeto(p1, p2)
+        return distance(p1, p2) < epsilon
+    end
+    # function to sort the checklist
+    function sortfunction(checklistentry)
+        return checklistentry[3]
+    end
+    # iterate over all sites
+    for s in 1:size(sites, 1)
+        # build a new checklist for sites
+        checklist = Array[]
+        # check all other sites in a lattice around this one
+        for i in -max_ij:max_ij
+        for j in -max_ij:max_ij
+        for alpha in 1:size(sites, 1)
+            # site at i*a1 + j*a2 + basis[alpha]
+            position = i.*lattice_vectors[1] .+ j.*lattice_vectors[2] .+ sites[alpha]
+            # check if the position is close to the original position
+            if closeto(position, sites[s])
+                # just the original site
+                continue
+            end
+            # put the site into the list
+            # format: [site_index, (wrap), distance]
+            push!(checklist, [alpha, (i,j), distance(position, sites[s])])
+        end
+        end
+        end
+        # sort the checklist
+        sort!(checklist, by=sortfunction)
+        # evaluate the checklist
+        # first: find out the number of nearest neighbors
+        number_NN = 0
+        # if unrestricted: simply go by distance
+        if min_NN <= 0 && max_NN <= 0
+            for c in checklist
+                if closeto(c[3], checklist[1][3])
+                    number_NN = number_NN + 1
+                else
+                    break
+                end
+            end
+        elseif min_NN > 0 && max_NN <=0
+            number_NN = min_NN-1
+            for c in checklist[min_NN:end]
+                if closeto(c[3], checklist[min_NN][3])
+                    number_NN = number_NN + 1
+                else
+                    break
+                end
+            end
+        else   
+            if min_NN <= 0
+                min_NN = 1
+            end
+            number_NN = min_NN-1
+            for c in checklist[min_NN:max_NN]
+                if closeto(c[3], checklist[min_NN][3])
+                    number_NN = number_NN + 1
+                else
+                    break
+                end
+            end
+        end
+        println("number of nearest neighbors of site $(s): $(number_NN)")
+        if closeto(checklist[number_NN][3],checklist[number_NN+1][3])
+            println("cut at the wrong number of nearest neighbors, there are more that are equally close!")
+        end
+        # restrict the checklist to that length
+        checklist = checklist[1:number_NN]
+        # parse into connections
+        for c in checklist
+            # define a new connection of interaction strength type float or int
+            if typeof(strength_NN) == Float64 || typeof(strength_NN) == Int64
+                push!(connections, [s, c[1], strength_NN, c[2]])
+            elseif typeof(strength_NN) == String
+                # automatic connection geometry based strength
+                if strength_NN == "AUTO"
+                    push!(connections, [s, c[1], 1/c[3], c[2]])
+                end
+            else
+                println("unknown connection strength type: $(typeof(strength_NN))")
+                return
+            end
+        end
+    end
+    # correct all connections - i.e. find out if the returning connection is present, if not: add it
+    connections_additional = Array[]
+    for c in connections
+        # check if "-c" is present
+        found = false
+        for mc in connections
+            if c[1] == mc[2] && c[2] == mc[1] && c[3] == mc[3] && -c[4][1] == mc[4][1] && -c[4][2] == mc[4][2]
+                found = true
+            end
+        end
+        if !found
+            push!(connections_additional, [c[2], c[1], c[3], (-c[4][1], -c[4][2])])
+        end
+    end
+    # add all additional connections
+    for c in connections_additional
+        push!(connections, c)
+    end
+    # build a filename
+    if name == "AUTO"
+        name = "from_$(size(sites, 1))_sites"
+    end
+    filename = "$(FOLDER_UNITCELLS)2d_$(name)_unitcell.jld"
+    # build the finished unitcell object
+    unitcell = Unitcell(
+        lattice_vectors,
+        sites,
+        connections,
+        filename
+    )
+    # maybe save
+    if save
+        saveUnitcell(unitcell)
+    end
+    # return the unitcell
+    return unitcell
+end
+export getUnitcellFromSites2D
 
 
 
@@ -6001,10 +6140,12 @@ function printInteractionMatrixInformation(connections_inside, connections_perio
     println("\\end{equation*}")
 end
 function printInteractionMatrixInformation(lattice::Lattice)
+    println("Interaction matrix for lattice \"$(lattice.filename)\"")
     connections_inside, connections_periodic = getInteractionMatrixInformation(lattice)
     printInteractionMatrixInformation(connections_inside, connections_periodic)
 end
 function printInteractionMatrixInformation(unitcell::Unitcell)
+    println("Interaction matrix for unitcell \"$(unitcell.filename)\"")
     connections_inside, connections_periodic = getInteractionMatrixInformation(unitcell)
     printInteractionMatrixInformation(connections_inside, connections_periodic)
 end
