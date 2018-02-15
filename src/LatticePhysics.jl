e07a4bdab5f35077be0a3587fd7430eed5195d64
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-02-15 13:23:30 +0100

    added job directory creation function

--- src/LatticePhysics.jl
+++ src/LatticePhysics.jl
@@ -9510,62 +9510,6 @@ function calculateBandStructureOfStrip(
 end
 export calculateBandStructureOfStrip
 
-# SOME DEFAULT PATHS
-DEFAULT_PATH_FCC = Array[
-    ["gamma"; [0,0,0]],
-    ["X"; [2*pi, 0, 0]],
-    ["W"; [2*pi, pi, 0]],
-    ["L"; [pi, pi, pi]],
-    ["gamma"; [0,0,0]],
-    ["K"; [3*pi/2, 3*pi/2, 0]],
-    ["X"; [2*pi, 0, 0]]
-]
-export DEFAULT_PATH_FCC
-
-DEFAULT_PATH_TRIANGULAR = Array[
-    ["gamma"; [0,0]],
-    ["K"; [2*pi/sqrt(3.0), 2*pi/3]],
-    ["M"; [2*pi/sqrt(3.0), 0]],
-    ["gamma"; [0,0]]
-]
-export DEFAULT_PATH_TRIANGULAR
-
-DEFAULT_PATH_SQUAREOCTAGON_2 = Array[
-    ["gamma"; [0,0]],
-    ["K"; [2,  0].*(pi / (1.0 + 1.0/sqrt(2.0)))],
-    ["M"; [1, -1].*(pi / (1.0 + 1.0/sqrt(2.0)))],
-    ["gamma"; [0,0]]
-]
-export DEFAULT_PATH_SQUAREOCTAGON_2
-
-
-DEFAULT_PATH_SQUARE_LONG = Array[
-    ["M";     [pi,   0.0]],
-    ["Gamma"; [0.0,  0.0]],
-    ["K'";    [pi,   -pi]],
-    ["M";     [pi,   0.0]],
-    ["M'";    [0.0,   pi]],
-    ["K";     [pi,    pi]],
-    ["Gamma"; [0.0,  0.0]]
-]
-export DEFAULT_PATH_SQUARE_LONG
-
-DEFAULT_PATH_SQUARE_SHORT = Array[
-    ["Gamma"; [0.0,  0.0]],
-    ["M";     [pi,   0.0]],
-    ["K";     [pi,    pi]],
-    ["Gamma"; [0.0,  0.0]]
-]
-export DEFAULT_PATH_SQUARE_SHORT
-
-DEFAULT_PATH_SQUARE = DEFAULT_PATH_SQUARE_SHORT
-export DEFAULT_PATH_SQUARE
-
-
-
-
-
-
 
 # FULL 2D Bandstructure
 function calculateBandStructure2D(
@@ -9670,6 +9614,482 @@ function calculateBandStructure2D(
     return fig   
 end
 export calculateBandStructure2D
+
+
+
+
+
+
+# SOME DEFAULT PATHS
+DEFAULT_PATH_FCC = Array[
+    ["gamma"; [0,0,0]],
+    ["X"; [2*pi, 0, 0]],
+    ["W"; [2*pi, pi, 0]],
+    ["L"; [pi, pi, pi]],
+    ["gamma"; [0,0,0]],
+    ["K"; [3*pi/2, 3*pi/2, 0]],
+    ["X"; [2*pi, 0, 0]]
+]
+export DEFAULT_PATH_FCC
+
+DEFAULT_PATH_TRIANGULAR = Array[
+    ["gamma"; [0,0]],
+    ["K"; [2*pi/sqrt(3.0), 2*pi/3]],
+    ["M"; [2*pi/sqrt(3.0), 0]],
+    ["gamma"; [0,0]]
+]
+export DEFAULT_PATH_TRIANGULAR
+
+DEFAULT_PATH_SQUAREOCTAGON_2 = Array[
+    ["gamma"; [0,0]],
+    ["K"; [2,  0].*(pi / (1.0 + 1.0/sqrt(2.0)))],
+    ["M"; [1, -1].*(pi / (1.0 + 1.0/sqrt(2.0)))],
+    ["gamma"; [0,0]]
+]
+export DEFAULT_PATH_SQUAREOCTAGON_2
+
+
+DEFAULT_PATH_SQUARE_LONG = Array[
+    ["M";     [pi,   0.0]],
+    ["Gamma"; [0.0,  0.0]],
+    ["K'";    [pi,   -pi]],
+    ["M";     [pi,   0.0]],
+    ["M'";    [0.0,   pi]],
+    ["K";     [pi,    pi]],
+    ["Gamma"; [0.0,  0.0]]
+]
+export DEFAULT_PATH_SQUARE_LONG
+
+DEFAULT_PATH_SQUARE_SHORT = Array[
+    ["Gamma"; [0.0,  0.0]],
+    ["M";     [pi,   0.0]],
+    ["K";     [pi,    pi]],
+    ["Gamma"; [0.0,  0.0]]
+]
+export DEFAULT_PATH_SQUARE_SHORT
+
+DEFAULT_PATH_SQUARE = DEFAULT_PATH_SQUARE_SHORT
+export DEFAULT_PATH_SQUARE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#
+#   METHODS FOR JOB SCHEDULING
+#
+#-----------------------------------------------------------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#
+#   FUNCTION TO CREATE A JOB DIRECTORY FOR SCHEDULING
+#
+#   USAGE: Give a jobdirectory name and a main program to run as well as a default input file and the function creates
+#   a job directory containing all input files as well as two scripts to run the program in parallel or single core to
+#   calculate all input files.
+#   Usage should be in the following steps:
+#   1) provide an input file template of the form: <parameter name>\t<default value>
+#   2) provide a main program which can be called by a command similar to "PROGRAM_NAME INPUTFILE.txt"
+#   3) call this function to create job directory and all input files
+#   4) Enter job directory and run one of the two julia execution scripts
+#       - Single core execution one after another: "julia script_single_execution.jl"
+#       - MPI based multi-core execution: "mpirun -x PATH --host <host1, host2, host3,...> --map-by node -np <number of processes> nice -n19 julia script_MPI_execution.jl"
+#   5) collect output files
+#
+#
+#   Parameters (necessary):
+#   - directory_path:       The name of the directory to be created
+#   - input_file_template:  The input file template from which parameters are extracted
+#   - main_program_file:    The file which contains the main program to be run
+#
+#   Parameters (optional):
+#   - parameters:           A dictonary of parameter values to be used
+#   - paramters_use_ARGS:   Bool to determine if the ARGS string is searched for parameters
+#   - input_file_seperator: The seperator used for input file seperation
+#   - main_program_execution_command:   The command to launch the program PROGRAM with a given input file INPUTFILE. Default: julia PROGRAM INPUTFILE
+#   - files_to_copy:        A list of file that are copied to the destination folder as well
+#
+#-----------------------------------------------------------------------------------------------------------------------------
+function createJobDirectory(
+    directory_path::String,
+    input_file_template_name::String,
+    main_program_file::String
+    ;
+    parameters::Dict=Dict(),
+    paramters_use_ARGS::Bool=true,
+    input_file_seperator::String=":\t",
+    main_program_execution_command::String="julia PROGRAM INPUTFILE",
+    files_to_copy::Array{String,1}=String[]
+        )
+    
+
+    # -----------------
+    # START INFORMATION
+    # -----------------
+
+    # correct the directory input
+    if !endswith(directory_path, "/")
+        directory_path = "$(directory_path)/"
+    end
+    # Start: Print the directory path
+    println("Job directory \"$(directory_path)\" will be created")
+    # Print the input template filename
+    println("Input template is located at \"$(input_file_template_name)\"\n")
+
+    
+
+
+    # ---------------------------------------------------
+    # OBTAIN THE INPUT FILE TEMPLATE AND EXTRACT KEYWORDS
+    # ---------------------------------------------------
+
+    # Open the template file and read all lines
+    input_file_template         = open(input_file_template_name)
+    input_file_template_lines   = readlines(input_file_template)
+    close(input_file_template)
+
+    # Create a new list for all parameters
+    parameters_RAW              = Array[]
+    parameters_passed_values    = Array[]
+
+    # push in all relevant lines
+    for line in input_file_template_lines
+        line = strip(line)
+        if startswith(line, "#") || line==""
+            continue
+        end
+        push!(parameters_RAW, split(line, "\t"))
+    end
+
+    # print all parameters that are found
+    println("Found $(size(parameters_RAW, 1)) parameters in template")
+    for p in parameters_RAW
+        if length(p) == 1
+            println("- \"$(p[1])\" without default value")
+        elseif length(p) == 2
+            println("- \"$(p[1])\" with default value $(p[2])")
+        end
+    end
+    println("")
+
+
+
+
+    # ---------------------------------------------------
+    # OBTAIN THE CORRECT VALUES TO BE USED
+    # ---------------------------------------------------
+
+    # Iterate over all accessible parameters
+    for p in parameters_RAW
+
+        # list of values to use
+        values_to_use   = []
+
+        # OPTION 1, check in Command line arguments
+        if paramters_use_ARGS
+            # look through all command line arguments
+            for arg in ARGS
+                # check if it fulfils the structure
+                if startswith(arg, "$(p[1])=")
+                    # get the string representation
+                    arg_value = replace(arg, "$(p[1])=", "")
+                    # push to the list of values to use
+                    push!(values_to_use, arg_value)
+                    # break the loop
+                    break
+                end
+            end
+        end
+        
+        # OPTION 2, check in dictonary
+        if length(values_to_use)==0 && p[1] in keys(parameters)
+            # get the string representation
+            dict_value = parameters(p[1])
+            # push to the list of values to use
+            push!(values_to_use, dict_value)
+        end
+
+        # OPTION 3, check default values of template
+        if length(values_to_use)==0 && length(p) >= 2
+            # get the string representation
+            default_value = p[2]
+            # push to the list of values to use
+            push!(values_to_use, default_value)
+        end
+
+
+        # PARSING OF VALUES
+
+        # create a list with parsed entries
+        values_to_use_parsed = []
+        
+        # iterate over all values to be used
+        for v in values_to_use
+            # determine which type of value
+            if typeof(v) != String && typeof(v) != SubString{String}
+                # already parsed, push into array
+                push!(values_to_use_parsed, v)
+            elseif startswith(v, "[") && endswith(v, "]")
+                # list of format [el1, el2, el3, ...]
+                v = split(strip(v[2:end-1]), ",")
+                for i in 1:length(v)
+                    v[i] = strip(v[i])
+                    push!(values_to_use_parsed, parse(Float64,v[i]))
+                end
+            elseif startswith(v, "linspace(") && endswith(v, ")")
+                # list via linspace(start, end, steps)
+                v = split(v[10:end-1], ",")
+                for i in 1:length(v)
+                    v[i] = strip(v[i])
+                end
+                v = linspace(parse(Float64,v[1]),parse(Float64,v[2]),parse(Float64,v[3]))
+                for i in 1:length(v)
+                    push!(values_to_use_parsed, v[i])
+                end
+            else
+                # it is probably either a number or a string, try parsing to float
+                if isnull(tryparse(Float64,v))
+                    # not a float, just push in list as it is
+                    push!(values_to_use_parsed, v)
+                else
+                    # push parsed into list
+                    push!(values_to_use_parsed, parse(Float64,v))
+                end
+            end
+        end
+
+
+        # append the list of paramters to the global list
+        push!(parameters_passed_values, values_to_use_parsed)
+
+    end
+
+    # print all values that are found
+    println("Found values for parameters:")
+    for (p_index,p) in enumerate(parameters_RAW)
+        println("- \"$(p[1])\", $(length(parameters_passed_values[p_index])) values: $(parameters_passed_values[p_index])")
+    end
+    println("")
+
+
+
+
+
+
+    # -----------
+    # CREATE JOBS
+    # -----------
+
+    # create a new list of dictonaries which are the jobs
+    joblist = []
+    # add a dummy job without information
+    push!(joblist, Dict())
+
+    # iterate over all parameters
+    for p in 1:length(parameters_RAW)
+
+        # find out if single value or multiple values
+        if length(parameters_passed_values[p]) == 1
+            # only one value, all jobs get this value
+            for j in joblist
+                j["$(parameters_RAW[p][1])"] = parameters_passed_values[p][1]
+            end
+        elseif length(parameters_passed_values[p]) >= 1
+            # more than one value, every job gets copied for every value
+            joblist_backup = joblist
+            joblist = []
+            for j in joblist_backup
+                for i in 1:length(parameters_passed_values[p])
+                    j_tmp = copy(j)
+                    j_tmp["$(parameters_RAW[p][1])"] = parameters_passed_values[p][i]
+                    push!(joblist, j_tmp)
+                end
+            end
+        else
+            # PROBLEM: NO VALUE FOR PARAMETER, exlude from job
+            println("DANGER: Excluding parameter $(parameters_RAW[p][1]) from jobs because it has no value")
+        end
+        
+    end
+
+    # print the success
+    println("Job building is done, $(length(joblist)) jobs are constructed\n")
+
+    # check if only one job without parameters
+    if length(joblist) == 1 && length(keys(joblist[1])) == 0
+        println("Some mistake occured, only job has no parameters. Aborting now...\n")
+        return
+    end
+
+
+
+
+
+    # --------------------
+    # CREATE THE DIRECTORY
+    # --------------------
+
+    # check if the directory exists
+    if isdir(directory_path)
+        # Directory exists
+        println("Directory \"$(directory_path)\" already exists")
+        print("Delete and reconstruct? [y/n]: ")
+        answer = strip(readline()[end-1:end])
+        while !in(answer, ["y", "n"])
+            print("Delete and reconstruct? [y/n]: ")
+            answer = strip(readline()[end-1:end])
+        end
+        # depending on answer
+        if answer == "y"
+            # ask for recursive deletion
+            print("Delete recursively? [y/n]: ")
+            answer = strip(readline()[end-1:end])
+            while !in(answer, ["y", "n"])
+                print("Delete and reconstruct? [y/n]: ")
+                answer = strip(readline()[end-1:end])
+            end
+            if answer == "y"
+                # delete the complete directory
+                rm(directory_path, recursive=true)
+            else
+                # delete the complete directory
+                rm(directory_path, recursive=false)
+            end
+            # print
+            println("Deleted already existing directory $(directory_path)")
+            # create the directory
+            mkdir(directory_path)
+            println("Recreated directory $(directory_path)\n")
+
+        else
+            # abort the program
+            println("Not deleting directory, aborting program now...")
+            return
+        end
+    else
+        # Directory does not exist, create it
+        mkdir(directory_path)
+        println("Created directory $(directory_path)\n")
+    end
+
+
+
+
+    # ----------------------
+    # CREATE THE INPUT FILES
+    # ----------------------
+
+    # iterate over all jobs
+    for i in 1:length(joblist)
+        # dump job i to file input_i.txt
+        jobfilename = "input_$(i).txt"
+        f = open("$(directory_path)$(jobfilename)", "w")
+        # write entries of the dictonary into the file
+        for k in keys(joblist[i])
+            write(f, "$(k)$(input_file_seperator)$(joblist[i][k])\n")
+        end
+        # close the file
+        close(f)
+        # set the correct input filename in the job
+        joblist[i]["input_filename"] = jobfilename
+    end
+    # print the success
+    println("Created all $(length(joblist)) jobfiles\n")
+
+
+
+
+    # ------------------------------
+    # COPY / CREATE THE SCRIPT FILES
+    # ------------------------------    
+
+    # Copy main program file
+    cp(main_program_file, "$(directory_path)$(main_program_file)")
+    println("Copied main program file \"$(main_program_file)\"")
+
+    # Create single execution julia script
+    script_single_exe = open("$(directory_path)script_single_execution.jl", "w")
+    # write all lines
+    for i in 1:length(joblist)
+        # compose a line
+        line = "run(`$(main_program_execution_command)`)\n"
+        # change the program part
+        line = replace(line, "PROGRAM", main_program_file)
+        # change the input part
+        line = replace(line, "INPUTFILE", joblist[i]["input_filename"])
+        # set the correct input filename in the job
+        write(script_single_exe, line)
+    end   
+    # close the script
+    close(script_single_exe)
+    println("Created single execution script \"script_single_execution.jl\"")
+
+
+    # Create single execution julia script
+    script_MPI_exe = open("$(directory_path)script_MPI_execution.jl", "w")
+
+
+    # write all lines, starting with imports
+    write(script_MPI_exe, "# use with:\n")
+    write(script_MPI_exe, "# mpirun -x PATH --host <host1,host2,...> --map-by node -np <number of processes> nice -n19 julia script_MPI_execution.jl\n")
+    write(script_MPI_exe, "using MPI\n")
+    write(script_MPI_exe, "MPI.Init()\n")
+    # get the number of the MPI process
+    write(script_MPI_exe, "commworld = MPI.COMM_WORLD\n")
+    write(script_MPI_exe, "commrank  = MPI.Comm_rank(commworld)\n")
+    write(script_MPI_exe, "commsize  = MPI.Comm_size(commworld)\n")
+    write(script_MPI_exe, "max_i     = $(length(joblist))\n")
+    write(script_MPI_exe, "init_i    = commrank+1\n")
+    write(script_MPI_exe, "for i in init_i:commsize:max_i\n")
+    # compose main line    
+    line = "\trun(pipeline(`$(main_program_execution_command)`, stdout=\"bash_stdout_INPUTFILE\", stderr=\"bash_stderr_INPUTFILE\"))\n\tprintln(\"Done input file \\\"INPUTFILE\\\"\")\n"
+    # change the program part
+    line = replace(line, "PROGRAM", main_program_file)
+    # change the input part
+    line = replace(line, "INPUTFILE", "input_\$(i).txt")
+    # set the correct input filename in the job
+    write(script_MPI_exe, line)
+    write(script_MPI_exe, "end\n")
+    # Finalize MPI script
+    write(script_MPI_exe, "MPI.Finalize()\n")
+    # close the script
+    close(script_MPI_exe)
+    println("Created MPI execution script \"script_MPI_execution.jl\"")
+
+    # Copy additional files
+    for fn in files_to_copy
+        cp(fn, "$(directory_path)$(fn)")
+        println("Copied additional file \"$(fn)\"")
+    end
+
+end
+export createJobDirectory
+
+
+
 
 
 # MODULE END
