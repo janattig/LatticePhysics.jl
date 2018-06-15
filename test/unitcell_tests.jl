dc0c360079e4bb8822e141d574c5516fb9d23c68
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-06-15 13:16:38 +0200

    formatted unitcell tests into Base.testset

--- test/unitcell_tests.jl
+++ test/unitcell_tests.jl
@@ -4,6 +4,8 @@
 using LatticePhysics
 using Base.Test
 
+# begin the Unitcell testblock
+unitcell_testset = @testset "Unitcell tests" begin
 
 
 ################################################################################
@@ -12,29 +14,37 @@ using Base.Test
 #
 ################################################################################
 
-# print part of unitcell test
-println("--> testing unitcell basics (saving / loading / etc.)")
+# begin the testset
+@testset "General Unitcell functions" begin
+
+    # get a test unitcell
+    unitcell = getUnitcellHoneycomb()
+
+    # test saving
+    @test typeof(saveUnitcell(unitcell))==String
+    # test loading
+    @test typeof(loadUnitcell(unitcell))==String
+
+
+    # test printing information
+    @test printInfo(unitcell)==nothing
+    # test testing method
+    @test typeof(testUnitcell(unitcell, 2, 2))==Bool
 
 
 
-# get a test unitcell
-unitcell = getUnitcellHoneycomb()
+    # testing connectivity information
+    @test typeof(getConnectivityList(unitcell)) == Array{Array,1}
+    # testing connection information
+    @test typeof(getConnectionList(unitcell)) == Array{Array,1}
+    # testing connection strength information
+    @test typeof(getConnectionStrengthList(unitcell)) == Array{Any,1}
 
-# test saving
-@test typeof(saveUnitcell(unitcell))==String
-# test loading
-@test typeof(loadUnitcell(unitcell))==String
-
-
-# test printing information
-@test printInfo(unitcell)==nothing
-# test testing method
-@test typeof(testUnitcell(unitcell, 2, 2))==Bool
+# end the testset here
+end;
 
 
 
-# empty line
-println("")
 
 
 
@@ -46,137 +56,141 @@ println("")
 #
 ################################################################################
 
-# print part of unitcell test
-println("--> testing already implemented unitcells")
+# begin the testset
+@testset "Implemented Unitcells in 2D" begin
 
+    #   2D UNITCELLS
+    #   - SQUARE / RECTANGLE
+    #       - getUnitcellSquare
+    @test testUnitcell(getUnitcellSquare(), 2, 2)
+    @test testUnitcell(getUnitcellSquare(1), 2, 2)
+    @test testUnitcell(getUnitcellSquare(2), 2, 2)
+    @test testUnitcell(getUnitcellSquare(3), 2, 2)
+    #       - getUnitcellExtendedSquare
+    @test testUnitcell(getUnitcellExtendedSquare(), 2, 2)
+    @test testUnitcell(getUnitcellExtendedSquare(1), 2, 2)
+    #       - getUnitcellCheckerboard
+    @test testUnitcell(getUnitcellCheckerboard(), 2, 2)
+    @test testUnitcell(getUnitcellCheckerboard(1), 2, 2)
+    #       - getUnitcellShastrySutherland
+    @test testUnitcell(getUnitcellShastrySutherland(), 2, 2)
+    @test testUnitcell(getUnitcellShastrySutherland(1), 2, 2)
+    @test testUnitcell(getUnitcellShastrySutherland(4), 2, 2)
+    #       - getUnitcellAdvancedSquare
+    @test testUnitcell(getUnitcellAdvancedSquare(), 2, 2)
+    @test testUnitcell(getUnitcellAdvancedSquare(1), 2, 2)
+    #       - getUnitcellSquareOctagon
+    @test testUnitcell(getUnitcellSquareOctagon(), 2, 2)
+    @test testUnitcell(getUnitcellSquareOctagon(1), 2, 2)
+    @test testUnitcell(getUnitcellSquareOctagon(4), 2, 2)
+    @test testUnitcell(getUnitcellSquareOctagon(2), 2, 2)
+    @test testUnitcell(getUnitcellSquareOctagon(5), 2, 2)
+    #       - getUnitcellBCC2D
+    @test testUnitcell(getUnitcellBCC2D(), 2, 2)
+    @test testUnitcell(getUnitcellBCC2D(1), 2, 2)
+    #       - getUnitcellFullyConnectedSquare
+    @test testUnitcell(getUnitcellFullyConnectedSquare(), 2, 2)
+    @test testUnitcell(getUnitcellFullyConnectedSquare(1), 2, 2)
 
-#   2D UNITCELLS
-#   - SQUARE / RECTANGLE
-#       - getUnitcellSquare
-@test testUnitcell(getUnitcellSquare(), 2, 2)
-@test testUnitcell(getUnitcellSquare(1), 2, 2)
-@test testUnitcell(getUnitcellSquare(2), 2, 2)
-@test testUnitcell(getUnitcellSquare(3), 2, 2)
-#       - getUnitcellExtendedSquare
-@test testUnitcell(getUnitcellExtendedSquare(), 2, 2)
-@test testUnitcell(getUnitcellExtendedSquare(1), 2, 2)
-#       - getUnitcellCheckerboard
-@test testUnitcell(getUnitcellCheckerboard(), 2, 2)
-@test testUnitcell(getUnitcellCheckerboard(1), 2, 2)
-#       - getUnitcellShastrySutherland
-@test testUnitcell(getUnitcellShastrySutherland(), 2, 2)
-@test testUnitcell(getUnitcellShastrySutherland(1), 2, 2)
-@test testUnitcell(getUnitcellShastrySutherland(4), 2, 2)
-#       - getUnitcellAdvancedSquare
-@test testUnitcell(getUnitcellAdvancedSquare(), 2, 2)
-@test testUnitcell(getUnitcellAdvancedSquare(1), 2, 2)
-#       - getUnitcellSquareOctagon
-@test testUnitcell(getUnitcellSquareOctagon(), 2, 2)
-@test testUnitcell(getUnitcellSquareOctagon(1), 2, 2)
-@test testUnitcell(getUnitcellSquareOctagon(4), 2, 2)
-@test testUnitcell(getUnitcellSquareOctagon(2), 2, 2)
-@test testUnitcell(getUnitcellSquareOctagon(5), 2, 2)
-#       - getUnitcellBCC2D
-@test testUnitcell(getUnitcellBCC2D(), 2, 2)
-@test testUnitcell(getUnitcellBCC2D(1), 2, 2)
-#       - getUnitcellFullyConnectedSquare
-@test testUnitcell(getUnitcellFullyConnectedSquare(), 2, 2)
-@test testUnitcell(getUnitcellFullyConnectedSquare(1), 2, 2)
+    #   - TRIANGULAR
+    #       - getUnitcellTriangular
+    @test testUnitcell(getUnitcellTriangular(), 2, 2)
+    @test testUnitcell(getUnitcellTriangular(1), 2, 2)
+    @test testUnitcell(getUnitcellTriangular(3), 2, 2)
+    #       - getUnitcellHoneycomb
+    @test testUnitcell(getUnitcellHoneycomb(), 2, 2)
+    @test testUnitcell(getUnitcellHoneycomb(1), 2, 2)
+    @test testUnitcell(getUnitcellHoneycomb(4), 2, 2)
+    @test testUnitcell(getUnitcellHoneycomb(2), 2, 2)
+    @test testUnitcell(getUnitcellHoneycomb(5), 2, 2)
+    @test testUnitcell(getUnitcellHoneycomb(3), 2, 2)
+    #       - getUnitcellKagome
+    @test testUnitcell(getUnitcellKagome(), 2, 2)
+    @test testUnitcell(getUnitcellKagome(1), 2, 2)
+    #       - getUnitcellKagomeMinus
+    @test testUnitcell(getUnitcellKagomeMinus(), 2, 2)
+    @test testUnitcell(getUnitcellKagomeMinus(1), 2, 2)
+    @test testUnitcell(getUnitcellKagomeMinus(4), 2, 2)
+    #       - getUnitcellHoneycombXXX
+    @test testUnitcell(getUnitcellHoneycombXXX(), 2, 2)
+    @test testUnitcell(getUnitcellHoneycombXXX(1), 2, 2)
+    @test testUnitcell(getUnitcellHoneycombXXX(2), 2, 2)
 
-
-#   - TRIANGULAR
-#       - getUnitcellTriangular
-@test testUnitcell(getUnitcellTriangular(), 2, 2)
-@test testUnitcell(getUnitcellTriangular(1), 2, 2)
-@test testUnitcell(getUnitcellTriangular(3), 2, 2)
-#       - getUnitcellHoneycomb
-@test testUnitcell(getUnitcellHoneycomb(), 2, 2)
-@test testUnitcell(getUnitcellHoneycomb(1), 2, 2)
-@test testUnitcell(getUnitcellHoneycomb(4), 2, 2)
-@test testUnitcell(getUnitcellHoneycomb(2), 2, 2)
-@test testUnitcell(getUnitcellHoneycomb(5), 2, 2)
-@test testUnitcell(getUnitcellHoneycomb(3), 2, 2)
-#       - getUnitcellKagome
-@test testUnitcell(getUnitcellKagome(), 2, 2)
-@test testUnitcell(getUnitcellKagome(1), 2, 2)
-#       - getUnitcellKagomeMinus
-@test testUnitcell(getUnitcellKagomeMinus(), 2, 2)
-@test testUnitcell(getUnitcellKagomeMinus(1), 2, 2)
-@test testUnitcell(getUnitcellKagomeMinus(4), 2, 2)
-#       - getUnitcellHoneycombXXX
-@test testUnitcell(getUnitcellHoneycombXXX(), 2, 2)
-@test testUnitcell(getUnitcellHoneycombXXX(1), 2, 2)
-@test testUnitcell(getUnitcellHoneycombXXX(2), 2, 2)
-
-
-
-
-
-#   3D UNITCELLS
-#   - CUBIC / FCC
-#       - getUnitcellDiamond
-@test testUnitcell(getUnitcellDiamond(), 3, 3)
-@test testUnitcell(getUnitcellDiamond(1), 3, 3)
-@test testUnitcell(getUnitcellDiamond(2), 3, 3)
-@test testUnitcell(getUnitcellDiamond(3), 3, 3)
-#       - getUnitcellBCC
-@test testUnitcell(getUnitcellBCC(), 3, 3)
-@test testUnitcell(getUnitcellBCC(1), 3, 3)
-#       - getUnitcellPyrochlore
-@test testUnitcell(getUnitcellPyrochlore(), 3, 3)
-@test testUnitcell(getUnitcellPyrochlore(1), 3, 3)
-
-#   - (X,3)y FAMILY
-#       - getUnitcell_8_3_a
-@test testUnitcell(getUnitcell_8_3_a(), 3, 3)
-@test testUnitcell(getUnitcell_8_3_a(1), 3, 3)
-@test testUnitcell(getUnitcell_8_3_a(4), 3, 3)
-#       - getUnitcell_8_3_b
-@test testUnitcell(getUnitcell_8_3_b(), 3, 3)
-@test testUnitcell(getUnitcell_8_3_b(1), 3, 3)
-@test testUnitcell(getUnitcell_8_3_b(4), 3, 3)
-#       - getUnitcell_8_3_c
-@test testUnitcell(getUnitcell_8_3_c(), 3, 3)
-@test testUnitcell(getUnitcell_8_3_c(1), 3, 3)
-@test testUnitcell(getUnitcell_8_3_c(4), 3, 3)
-#       - getUnitcell_8_3_n
-@test testUnitcell(getUnitcell_8_3_n(), 3, 3)
-@test testUnitcell(getUnitcell_8_3_n(1), 3, 3)
-@test testUnitcell(getUnitcell_8_3_n(4), 3, 3)
-#       - getUnitcell_9_3_a
-@test testUnitcell(getUnitcell_9_3_a(), 3, 3)
-@test testUnitcell(getUnitcell_9_3_a(1), 3, 3)
-@test testUnitcell(getUnitcell_9_3_a(4), 3, 3)
-@test testUnitcell(getUnitcell_9_3_a(2), 3, 3)
-@test testUnitcell(getUnitcell_9_3_a(5), 3, 3)
-#       - getUnitcell_10_3_a / getUnitcellHyperoctagon
-@test testUnitcell(getUnitcell_10_3_a(), 3, 3)
-@test testUnitcell(getUnitcell_10_3_a(1), 3, 3)
-@test testUnitcell(getUnitcell_10_3_a(4), 3, 3)
-@test testUnitcell(getUnitcellHyperoctagon(), 3, 3)
-@test testUnitcell(getUnitcellHyperoctagon(1), 3, 3)
-@test testUnitcell(getUnitcellHyperoctagon(4), 3, 3)
-#       - getUnitcell_10_3_b / getUnitcellHyperhoneycomb
-@test testUnitcell(getUnitcell_10_3_b(), 3, 3)
-@test testUnitcell(getUnitcell_10_3_b(1), 3, 3)
-@test testUnitcell(getUnitcell_10_3_b(4), 3, 3)
-@test testUnitcell(getUnitcell_10_3_b(2), 3, 3)
-@test testUnitcell(getUnitcell_10_3_b(5), 3, 3)
-@test testUnitcell(getUnitcellHyperhoneycomb(), 3, 3)
-@test testUnitcell(getUnitcellHyperhoneycomb(1), 3, 3)
-@test testUnitcell(getUnitcellHyperhoneycomb(4), 3, 3)
-@test testUnitcell(getUnitcellHyperhoneycomb(2), 3, 3)
-@test testUnitcell(getUnitcellHyperhoneycomb(5), 3, 3)
-#       - getUnitcell_10_3_c
-@test testUnitcell(getUnitcell_10_3_c(), 3, 3)
-@test testUnitcell(getUnitcell_10_3_c(1), 3, 3)
-@test testUnitcell(getUnitcell_10_3_c(4), 3, 3)
-#       - getUnitcell_10_3_d
-@test testUnitcell(getUnitcell_10_3_d(), 3, 3)
-@test testUnitcell(getUnitcell_10_3_d(1), 3, 3)
-@test testUnitcell(getUnitcell_10_3_d(4), 3, 3)
+# end the testset here
+end;
 
 
 
-# empty line
-println("")
+# begin the testset
+@testset "Implemented Unitcells in 3D" begin
+
+    #   3D UNITCELLS
+    #   - CUBIC / FCC
+    #       - getUnitcellDiamond
+    @test testUnitcell(getUnitcellDiamond(), 3, 3)
+    @test testUnitcell(getUnitcellDiamond(1), 3, 3)
+    @test testUnitcell(getUnitcellDiamond(2), 3, 3)
+    @test testUnitcell(getUnitcellDiamond(3), 3, 3)
+    #       - getUnitcellBCC
+    @test testUnitcell(getUnitcellBCC(), 3, 3)
+    @test testUnitcell(getUnitcellBCC(1), 3, 3)
+    #       - getUnitcellPyrochlore
+    @test testUnitcell(getUnitcellPyrochlore(), 3, 3)
+    @test testUnitcell(getUnitcellPyrochlore(1), 3, 3)
+
+    #   - (X,3)y FAMILY
+    #       - getUnitcell_8_3_a
+    @test testUnitcell(getUnitcell_8_3_a(), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_a(1), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_a(4), 3, 3)
+    #       - getUnitcell_8_3_b
+    @test testUnitcell(getUnitcell_8_3_b(), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_b(1), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_b(4), 3, 3)
+    #       - getUnitcell_8_3_c
+    @test testUnitcell(getUnitcell_8_3_c(), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_c(1), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_c(4), 3, 3)
+    #       - getUnitcell_8_3_n
+    @test testUnitcell(getUnitcell_8_3_n(), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_n(1), 3, 3)
+    @test testUnitcell(getUnitcell_8_3_n(4), 3, 3)
+    #       - getUnitcell_9_3_a
+    @test testUnitcell(getUnitcell_9_3_a(), 3, 3)
+    @test testUnitcell(getUnitcell_9_3_a(1), 3, 3)
+    @test testUnitcell(getUnitcell_9_3_a(4), 3, 3)
+    @test testUnitcell(getUnitcell_9_3_a(2), 3, 3)
+    @test testUnitcell(getUnitcell_9_3_a(5), 3, 3)
+    #       - getUnitcell_10_3_a / getUnitcellHyperoctagon
+    @test testUnitcell(getUnitcell_10_3_a(), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_a(1), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_a(4), 3, 3)
+    @test testUnitcell(getUnitcellHyperoctagon(), 3, 3)
+    @test testUnitcell(getUnitcellHyperoctagon(1), 3, 3)
+    @test testUnitcell(getUnitcellHyperoctagon(4), 3, 3)
+    #       - getUnitcell_10_3_b / getUnitcellHyperhoneycomb
+    @test testUnitcell(getUnitcell_10_3_b(), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_b(1), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_b(4), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_b(2), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_b(5), 3, 3)
+    @test testUnitcell(getUnitcellHyperhoneycomb(), 3, 3)
+    @test testUnitcell(getUnitcellHyperhoneycomb(1), 3, 3)
+    @test testUnitcell(getUnitcellHyperhoneycomb(4), 3, 3)
+    @test testUnitcell(getUnitcellHyperhoneycomb(2), 3, 3)
+    @test testUnitcell(getUnitcellHyperhoneycomb(5), 3, 3)
+    #       - getUnitcell_10_3_c
+    @test testUnitcell(getUnitcell_10_3_c(), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_c(1), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_c(4), 3, 3)
+    #       - getUnitcell_10_3_d
+    @test testUnitcell(getUnitcell_10_3_d(), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_d(1), 3, 3)
+    @test testUnitcell(getUnitcell_10_3_d(4), 3, 3)
+
+# end the testset here
+end;
+
+
+# end the unitcell test block here
+end;
