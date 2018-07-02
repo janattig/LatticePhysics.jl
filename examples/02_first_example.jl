6c6fa07d6e84d944c454ab905bfb38a3459313b8
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-07-02 12:20:48 +0200

    updated the present example

--- examples/02_first_example.jl
+++ examples/02_first_example.jl
@@ -7,9 +7,9 @@ unitcell = getUnitcellHoneycomb()
 # build a lattice for visual confirmation that this is the correct unitcell
 lattice = getLatticeInBox(unitcell, [10.0, 8.0])
 # plot the lattice for visual confirmation
-plotLattice(lattice, openfile=true)
+plotLattice2D(lattice, openfile=true)
 
 # get a path for band structure calculations, use the default path
-path = DEFAULT_PATH_TRIANGULAR
+path = getDefaultPathTriangular()
 # calculate the band structure (using the unitcell)
-calculateBandStructureAlongPath(unitcell, path, showPlot=true)
+plotBandstructure(unitcell, path, showPlot=true)
