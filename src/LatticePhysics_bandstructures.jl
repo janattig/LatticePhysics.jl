a5784aa22e52c82f97274860b841ed940854a273
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-07-16 11:10:32 +0200

    changed some band structure definition

--- src/LatticePhysics_bandstructures.jl
+++ src/LatticePhysics_bandstructures.jl
@@ -65,8 +65,14 @@ export Bandstructure
                 enforce_hermitian::Bool=false ]
             )
 
-Calculates the band struture of a `Unitcell` object along some path given by a `Path` object
-and returns a `Bandstructure` object.
+    getBandStructureAlongPath(
+                matrixFunction::Function,
+                path::Path
+             [; resolution::Int64=-1 ]
+            )
+
+Calculates the band struture of a `Unitcell` object (or a matrix given by a function `matrixFunction`)
+along some path given by a `Path` object and returns a `Bandstructure` object.
 
 
 # Examples
@@ -120,6 +126,63 @@ function getBandStructureAlongPath(
             k = (k2 .* multipliers[i]) .+ (k1 .* (1-multipliers[i]))
             # get the interaction matrix for this k
             matrix = getInteractionMatrixKSpace(unitcell, k, enforce_hermitian=enforce_hermitian)
+            # diagonalize the matrix
+            eigenvalues = eigvals(matrix)
+            # save all the eigenvalues to their lists
+            for b in 1:length(unitcell.basis)
+                segments_total[s][b][i] = eigenvalues[b]
+            end
+        end
+    end
+
+    # generate a new band structure object
+    bandstructure = Bandstructure(path, segments_total)
+
+    # return the band structure
+    return bandstructure
+end
+function getBandStructureAlongPath(
+                matrixFunction::Function,
+                path::Path;
+                resolution::Int64=-1
+            )
+
+    # maybe modify the path resolution
+    if resolution > 0
+        setTotalResolution!(path, resolution)
+    end
+
+    # build up the list of all bands of all segments
+    segments_total = Array{Array{Float64,1},1}[]
+    # get the number of bands
+    N_bands = size(matrixFunction(path.points[1]),1)
+    # iterate over all path segments and push empty lists into the segments list
+    for i in 1:length(path.segment_resolution)
+        # build an empty band structure for this segment
+        segment = Array{Float64, 1}[]
+        for b in 1:N_bands
+            push!(segment, zeros(Float64, path.segment_resolution[i]))
+        end
+        # push the segment band structure into the complete segment list
+        push!(segments_total, segment)
+    end
+
+
+    # iterate over all segments
+    for s in 1:length(path.segment_resolution)
+        # get the grid in between two points
+        segment_resolution = path.segment_resolution[s]
+        # get all multipliers of k vectors (i.e. all alpha in (1-alpha)*k_1 + alpha*k_2)
+        multipliers = linspace(0, 1, segment_resolution)
+        # get the local start and end point of the segment
+        k1 = path.points[s]
+        k2 = path.points[s+1]
+        # calculate all energies
+        for i in 1:segment_resolution
+            # get the current k
+            k = (k2 .* multipliers[i]) .+ (k1 .* (1-multipliers[i]))
+            # get the interaction matrix for this k
+            matrix = matrixFunction(k)
             # diagonalize the matrix
             eigenvalues = eigvals(matrix)
             # save all the eigenvalues to their lists
