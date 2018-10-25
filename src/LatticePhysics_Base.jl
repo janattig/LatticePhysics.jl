70d07546460f7b66da9aaab6eb8adc431877eead
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-25 15:28:55 +0200

    added LatticePhysics_Base module in separate file

---
+++ src/LatticePhysics_Base.jl
@@ -0,0 +1,50 @@
+################################################################################
+#
+#   module LatticePhysics_Base
+#
+#   --> ABSTRACT TYPE DEFINITION
+#           - AbstractSite{D,L}
+#           - AbstractBond{N,L}
+#           - AbstractUnitcell{D,N,L, S,B}
+#           - AbstractLattice{D,N,L, S,B}
+#
+#   --> INTERFACING ABSTRACT TYPES
+#           - AbstractSite{D,L}
+#               - positions
+#               - label
+#           - AbstractBond{N,L}
+#               - connecting identifiers
+#               - label
+#               - wrap
+#           - AbstractUnitcell{D,N,L}
+#               - Bravais lattice vectors
+#               - sites
+#               - bonds
+#           - AbstractLattice{D,N,L}
+#               - Unitcell
+#               - Bravais lattice vectors
+#               - sites
+#               - bonds
+#
+#   --> NAIVE STRUCT DEFINITIONS
+#           - Site{D,L}
+#           - Bond{N,L}
+#           - Unitcell{D,N,L}
+#           - Lattice{D,N,L}
+#
+#   --> IMPLEMENTING JULIA.BASE FUNCTIONS
+#           - show
+#           - print
+#
+################################################################################
+
+# module start
+module LatticePhysics_Base
+
+
+
+
+
+
+# module end
+end
