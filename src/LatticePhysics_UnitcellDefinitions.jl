712ca5f42e40622a97faf7af60568dd18a2e23d3
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-26 13:58:02 +0200

    added files for 2d and 3d unitcell implementations

---
+++ src/LatticePhysics_UnitcellDefinitions.jl
@@ -0,0 +1,25 @@
+################################################################################
+#
+#   module LatticePhysics_UnitcellDefinitions
+#   -> LatticePhysics_Base
+#   -> LinearAlgebra
+#
+#   --> PRE-IMPLEMENTED UNITCELLS 2D
+#
+#   --> PRE-IMPLEMENTED UNITCELLS 3D
+#
+################################################################################
+
+
+# module start
+module LatticePhysics_UnitcellDefinitions
+
+
+# include 2d stuff
+
+# include 3d stuff
+
+
+
+# module end
+module end
