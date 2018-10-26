ad3aeecc75b15b0f45cf222881f2132d3386e035
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-26 14:49:25 +0200

    added string labels for unitcell square

--- src/LatticePhysics_UnitcellDefinitions_2d.jl
+++ src/LatticePhysics_UnitcellDefinitions_2d.jl
@@ -45,3 +45,38 @@ function getUnitcellSquare(
         error("Version " * string(version) * " of square lattice unitcell for label type " * string(L) * " not implemented yet")
     end
 end
+
+# Implementation with String label types
+function getUnitcellSquare(
+            label_type :: Type{L};
+            version :: Int64 = 1,
+            unitcell_type :: Type{U} = Unitcell{2,2,L,Site{L,2},Bond{L,2}}
+        ) :: U where {L<:AbstractString,S<:AbstractSite{L,2},B<:AbstractBond{L,2},U<:AbstractUnitcell{2,2,L,S,B}}
+    # return a different unitcell depending on version
+    if version == 1
+        # return a new Unitcell
+        return newUnitcell(
+            # lattice vectors
+            Vector{Float64}[
+                Float64[1, 0],
+                Float64[0, 1]
+            ],
+            # sites
+            S[
+                newSite(Float64[0,0], L("1"), S)
+            ],
+            # bonds
+            B[
+                newBond(1,1, L("1"), (+1,0), B),
+                newBond(1,1, L("1"), (-1,0), B),
+                newBond(1,1, L("1"), (0,+1), B),
+                newBond(1,1, L("1"), (0,-1), B)
+            ],
+            # give the unitcell type
+            U
+        )
+    else
+        # give an error
+        error("Version " * string(version) * " of square lattice unitcell for label type " * string(L) * " not implemented yet")
+    end
+end
