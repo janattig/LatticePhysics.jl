5c651b6da1ceb105a716c5f1e332ec302c8292af
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-26 11:30:51 +0200

    added newUnitcell interface

--- src/LatticePhysics_Base_abstract_unitcell.jl
+++ src/LatticePhysics_Base_abstract_unitcell.jl
@@ -22,4 +22,32 @@
 #   ABSTRACT TYPE DEFINITION
 #
 ################################################################################
-abstract type AbstractUnitcell{D,N,L,B<:AbstractBond{L,N},S<:AbstractSite{L,D}} end
+abstract type AbstractUnitcell{D,N,L,S<:AbstractSite{L,D},B<:AbstractBond{L,N}} end
+
+
+
+
+
+
+################################################################################
+#
+#	INTERFACING / ACCESSING UNITCELLS
+#	(functions have to be overwritten by concrete types)
+#
+################################################################################
+
+
+# default constructor interface
+# used for creation of new unitcells
+function newUnitcell(
+            lattice_vectors :: Vector{<:Real},
+            sites           :: Vector{S},
+            bonds           :: Vector{B},
+            ::Type{U}
+        ) :: U where {D,N,L,S<:AbstractSite{L,D},B<:AbstractBond{L,N},U<:AbstractUnitcell{D,N,L,S,B}}
+
+    # print an error because implementation for concrete type is missing
+    error(  "not implemented function 'newUnitcell' for concrete unitcell type " *
+            string(U) * " with bond type " * string(B) *
+            " and site type " * string(S)   )
+end
