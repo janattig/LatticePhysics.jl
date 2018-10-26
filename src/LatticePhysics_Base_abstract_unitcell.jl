0521fc416a361053e3097dc667f662a1472850aa
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-26 11:24:23 +0200

    added abstract type definition of unitcell

--- src/LatticePhysics_Base_abstract_unitcell.jl
+++ src/LatticePhysics_Base_abstract_unitcell.jl
@@ -1,3 +1,25 @@
+################################################################################
+#
+#	ABSTRACT TYPE
+#
+#   Unitcell{D,N,L,B,S}
+#   --> D is the dimension of embedding space
+#   --> N is the dimension of the Bravais lattice that the bond is located in
+#   --> L is the label type
+#   --> B is the bond type (<: AbstractBond{L,N})
+#   --> S is the site type (<: AbstractSite{L,D})
+#
+#   FILE CONTAINS
+#       - abstract type definition
+#       - interface definition
+#       - interface testing
+#
+################################################################################
 
-# Unitcells
+
+################################################################################
+#
+#   ABSTRACT TYPE DEFINITION
+#
+################################################################################
 abstract type AbstractUnitcell{D,N,L,B<:AbstractBond{L,N},S<:AbstractSite{L,D}} end
