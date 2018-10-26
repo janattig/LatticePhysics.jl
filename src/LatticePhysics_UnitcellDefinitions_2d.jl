d8a8898a4be3324fc42847551a58e8298b1f6170
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-26 14:45:31 +0200

    fixed unitcell implementation for square lattice

--- src/LatticePhysics_UnitcellDefinitions_2d.jl
+++ src/LatticePhysics_UnitcellDefinitions_2d.jl
@@ -1,6 +1,6 @@
 # SQUARE LATTICE
 
-# Implementation with unknown label type
+# Implementation with unknown label type --> raise error
 function getUnitcellSquare(
             label_type :: Type{L};
             version :: Int64 = 1,
@@ -11,12 +11,12 @@ function getUnitcellSquare(
     error("Label type " * string(L) * " of square lattice unitcell not implemented yet")
 end
 
-# Implementation with Int64 label type
+# Implementation with numbered label types (DEFAULT: Int64)
 function getUnitcellSquare(
-            label_type :: Type{Int64};
+            label_type :: Type{L} = Int64;
             version :: Int64 = 1,
-            unitcell_type :: Type{U} = Unitcell{2,2,Int64,Site{Int64,2},Bond{Int64,2}}
-        ) :: U where {S<:AbstractSite{Int64,2},B<:AbstractBond{Int64,2},U<:AbstractUnitcell{2,2,Int64,S,B}}
+            unitcell_type :: Type{U} = Unitcell{2,2,L,Site{L,2},Bond{L,2}}
+        ) :: U where {L<:Number,S<:AbstractSite{L,2},B<:AbstractBond{L,2},U<:AbstractUnitcell{2,2,L,S,B}}
     # return a different unitcell depending on version
     if version == 1
         # return a new Unitcell
@@ -28,20 +28,20 @@ function getUnitcellSquare(
             ],
             # sites
             S[
-                newSite(Float64[0,0], 1, S)
+                newSite(Float64[0,0], L(1), S)
             ],
             # bonds
             B[
-                newBond(1,1, 1, (+1,0), B),
-                newBond(1,1, 1, (-1,0), B),
-                newBond(1,1, 1, (0,+1), B),
-                newBond(1,1, 1, (0,-1), B)
+                newBond(1,1, L(1), (+1,0), B),
+                newBond(1,1, L(1), (-1,0), B),
+                newBond(1,1, L(1), (0,+1), B),
+                newBond(1,1, L(1), (0,-1), B)
             ],
             # give the unitcell type
             U
         )
     else
         # give an error
-        error("Version " * string(version) * " of square lattice unitcell for label type Int64 not implemented yet")
+        error("Version " * string(version) * " of square lattice unitcell for label type " * string(L) * " not implemented yet")
     end
 end
