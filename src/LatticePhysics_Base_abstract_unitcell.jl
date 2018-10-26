57ad53a7cbb3af29569a2331b7ca6955f56dc508
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-10-26 11:38:43 +0200

    added more interface functions to unitcell definition

--- src/LatticePhysics_Base_abstract_unitcell.jl
+++ src/LatticePhysics_Base_abstract_unitcell.jl
@@ -40,7 +40,7 @@ abstract type AbstractUnitcell{D,N,L,S<:AbstractSite{L,D},B<:AbstractBond{L,N}} 
 # default constructor interface
 # used for creation of new unitcells
 function newUnitcell(
-            lattice_vectors :: Vector{<:Real},
+            lattice_vectors :: Vector{Vector{<:Real}},
             sites           :: Vector{S},
             bonds           :: Vector{B},
             ::Type{U}
@@ -50,4 +50,39 @@ function newUnitcell(
     error(  "not implemented function 'newUnitcell' for concrete unitcell type " *
             string(U) * " with bond type " * string(B) *
             " and site type " * string(S)   )
+end
+
+
+
+
+
+# accessing a list of lattice vectors
+function latticeVectors(
+            unitcell :: U
+        ) :: Vector{Vector{Float64}} where {D,N,L,S<:AbstractSite{L,D},B<:AbstractBond{L,N},U<:AbstractUnitcell{D,N,L,S,B}}
+
+    # print an error because implementation for concrete type is missing
+    error(  "not implemented function 'latticeVectors' for concrete unitcell type " * string(U) )
+end
+
+
+# accessing a list of sites
+function sites(
+            unitcell :: U
+        ) :: Vector{S} where {D,N,L,S<:AbstractSite{L,D},B<:AbstractBond{L,N},U<:AbstractUnitcell{D,N,L,S,B}}
+
+    # print an error because implementation for concrete type is missing
+    error(  "not implemented function 'sites' for concrete unitcell type " *
+            string(U) * " with site type " * string(S)   )
+end
+
+
+# accessing a list of bonds
+function bonds(
+            unitcell :: U
+        ) :: Vector{B} where {D,N,L,S<:AbstractSite{L,D},B<:AbstractBond{L,N},U<:AbstractUnitcell{D,N,L,S,B}}
+
+    # print an error because implementation for concrete type is missing
+    error(  "not implemented function 'bonds' for concrete unitcell type " *
+            string(U) * " with bond type " * string(B)   )
 end
