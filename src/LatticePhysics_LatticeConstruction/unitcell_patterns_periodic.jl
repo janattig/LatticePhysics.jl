068c9faf3e2b68361f8d4793d4f45fc00712cc75
Jan <jattig@thp.uni-koeln.de>
2018-11-05 11:08:26 +0100

    some updates

--- src/LatticePhysics_LatticeConstruction/unitcell_patterns_periodic.jl
+++ src/LatticePhysics_LatticeConstruction/unitcell_patterns_periodic.jl
@@ -6,7 +6,7 @@
 function getLatticePeriodic(
         unitcell    :: U,
         extent      :: NTuple{2,Int64},
-        lattice_type:: Type{LA} = Lattice{D,L,2,Site{L,D},Bond{L,2}}
+        lattice_type:: Type{LA} = Lattice{D,L,2,S,B}
     ) :: LA where {D,L,S<:AbstractSite{L,D},B<:AbstractBond{L,2},U<:AbstractUnitcell{D,2,L,S,B},LA<:AbstractLattice{D,2,L,S,B}}
 
 end
