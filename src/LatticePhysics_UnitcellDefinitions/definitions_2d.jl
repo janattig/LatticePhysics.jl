42ca185272d7c4714e7a69645d2e22a5270828e4
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-07 13:22:12 +0100

    changed how 2d unitcell definitions are working

--- src/LatticePhysics_UnitcellDefinitions/definitions_2d.jl
+++ src/LatticePhysics_UnitcellDefinitions/definitions_2d.jl
@@ -3,82 +3,19 @@
 
 # Implementation with any other label type (Fallback)
 function getUnitcellSquare(
-            unitcell_type :: Type{U};
-            version :: Int64 = 1,
+            unitcell_type   :: Type{U};
+            version         :: Int64 = 1,
         ) :: U where {LS,LB,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
 
-    # give an error
-    error("Label types " * string(LS) * " / " * string(LB) * " for square lattice not implemented yet")
-end
-
-# Implementation with String label types
-function getUnitcellSquare(
-            unitcell_type :: Type{U};
-            version :: Int64 = 1,
-        ) :: U where {LS<:AbstractString,LB<:AbstractString,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
-    # return a different unitcell depending on version
-    if version == 1
-        # return a new Unitcell
-        return newUnitcell(
-            # lattice vectors
-            Vector{Float64}[
-                Float64[1, 0],
-                Float64[0, 1]
-            ],
-            # sites
-            S[
-                newSite(Float64[0,0], LS("1"), S)
-            ],
-            # bonds
-            B[
-                newBond(1,1, LB("1"), (+1,0), B),
-                newBond(1,1, LB("1"), (-1,0), B),
-                newBond(1,1, LB("1"), (0,+1), B),
-                newBond(1,1, LB("1"), (0,-1), B)
-            ],
-            # give the unitcell type
-            U
-        )
-    else
+    # this might fail due to missing implementation
+    try
+        # try to call the respective subfunction
+        return getUnitcellSquare(unitcell_type, Val{version})
+    catch Error
         # give an error
         error("Version " * string(version) * " of square lattice unitcell for label type " * string(LS) * " / " * string(LB) * " not implemented yet")
     end
 end
-
-# Implementation with Number label types
-function getUnitcellSquare(
-            unitcell_type :: Type{U};
-            version :: Int64 = 1,
-        ) :: U where {LS<:Number,LB<:Number,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
-    # return a different unitcell depending on version
-    if version == 1
-        # return a new Unitcell
-        return newUnitcell(
-            # lattice vectors
-            Vector{Float64}[
-                Float64[1, 0],
-                Float64[0, 1]
-            ],
-            # sites
-            S[
-                newSite(Float64[0,0], LS(1), S)
-            ],
-            # bonds
-            B[
-                newBond(1,1, LB(1), (+1,0), B),
-                newBond(1,1, LB(1), (-1,0), B),
-                newBond(1,1, LB(1), (0,+1), B),
-                newBond(1,1, LB(1), (0,-1), B)
-            ],
-            # give the unitcell type
-            U
-        )
-    else
-        # give an error
-        error("Version " * string(version) * " of square lattice unitcell for label type " * string(LS) * " / " * string(LB) * " not implemented yet")
-    end
-end
-
 
 # wrapper function for only passing the label type (DEFAULT: Int64)
 function getUnitcellSquare(
@@ -87,4 +24,69 @@ function getUnitcellSquare(
         ) :: Unitcell{Site{L,2},Bond{L,2}} where L
     # create a suitable unitcell of the given type
     return getUnitcellSquare(Unitcell{Site{L,2},Bond{L,2}}, version=version)
+end
+
+
+
+
+# Implementation
+# - version 1
+# - String labels
+function getUnitcellSquare(
+            unitcell_type :: Type{U},
+            version :: Type{Val{1}}
+        ) :: U where {LS<:AbstractString,LB<:AbstractString,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
+
+    # return a new Unitcell
+    return newUnitcell(
+        # lattice vectors
+        Vector{Float64}[
+            Float64[1, 0],
+            Float64[0, 1]
+        ],
+        # sites
+        S[
+            newSite(Float64[0,0], LS("1"), S)
+        ],
+        # bonds
+        B[
+            newBond(1,1, LB("1"), (+1,0), B),
+            newBond(1,1, LB("1"), (-1,0), B),
+            newBond(1,1, LB("1"), (0,+1), B),
+            newBond(1,1, LB("1"), (0,-1), B)
+        ],
+        # give the unitcell type
+        U
+    )
+end
+
+# Implementation
+# - version 1
+# - Number labels
+function getUnitcellSquare(
+            unitcell_type :: Type{U},
+            version :: Type{Val{1}}
+        ) :: U where {LS<:Number,LB<:Number,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
+
+    # return a new Unitcell
+    return newUnitcell(
+        # lattice vectors
+        Vector{Float64}[
+            Float64[1, 0],
+            Float64[0, 1]
+        ],
+        # sites
+        S[
+            newSite(Float64[0,0], LS(1), S)
+        ],
+        # bonds
+        B[
+            newBond(1,1, LB(1), (+1,0), B),
+            newBond(1,1, LB(1), (-1,0), B),
+            newBond(1,1, LB(1), (0,+1), B),
+            newBond(1,1, LB(1), (0,-1), B)
+        ],
+        # give the unitcell type
+        U
+    )
 end
