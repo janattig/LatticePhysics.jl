f5546f062676b744cae99d562360febf8442211e
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-07 14:39:48 +0100

    added some comments

--- src/LatticePhysics_UnitcellDefinitions/definitions_2d.jl
+++ src/LatticePhysics_UnitcellDefinitions/definitions_2d.jl
@@ -1,2 +1,13 @@
+################################################################################
+#
+#   DEFINITION OF 2D UNITCELLS
+#
+#   FOLLOWING UNITCELLS ARE IMPLEMENTED
+#   - square
+#
+#   Code is included in subfiles
+#
+################################################################################
+
 # Square lattice
 include("unitcells_2d/square.jl")
--- src/LatticePhysics_UnitcellDefinitions/definitions_3d.jl
+++ src/LatticePhysics_UnitcellDefinitions/definitions_3d.jl
@@ -1,2 +1,13 @@
+################################################################################
+#
+#   DEFINITION OF 3D UNITCELLS
+#
+#   FOLLOWING UNITCELLS ARE IMPLEMENTED
+#   - cubic
+#
+#   Code is included in subfiles
+#
+################################################################################
+
 # Cubic lattice
 include("unitcells_3d/cubic.jl")
