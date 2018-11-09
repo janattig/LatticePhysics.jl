36659eae44cc70a25a6d47ff731fe35e98c426d3
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-09 10:46:30 +0100

    finished creation of unitcell generating code

--- src/LatticePhysics_UnitcellConstruction/code_printing.jl
+++ src/LatticePhysics_UnitcellConstruction/code_printing.jl
@@ -1,5 +1,7 @@
 # PRINT  THE CODE THAT GENERATES A UNITCELL
-function printUnitcellGeneratingCode(
+
+# 2D unitcells (2 Bravais lattice vectors)
+function getUnitcellGeneratingCode(
             unitcell    :: U,
             name        :: String,
             version     :: Int64 = 1;
@@ -7,45 +9,78 @@ function printUnitcellGeneratingCode(
             labeltype_bond  :: DataType = Nothing,
             print_fallback  :: Bool     = true
         ) where {D,LS,LB,N, S<:AbstractSite{LS,D}, B<:AbstractBond{LB,N}, U<:AbstractUnitcell{S,B}}
+
     # set the correct label type for sites and bonds
     sitelabeltype = (labeltype_site == Nothing ? LS : labeltype_site) :: DataType
     bondlabeltype = (labeltype_bond == Nothing ? LB : labeltype_bond) :: DataType
+
+    # build the string of lattice vectors
+    lattice_vector_string = ""
+    for l in latticeVectors(unitcell)
+        lattice_vector_string = lattice_vector_string * "            Float64" * string(l) * ",\n"
+    end
+    if length(lattice_vector_string) > 2
+        lattice_vector_string = lattice_vector_string[1:end-2] * "\n"
+    end
+
+    # build a string for all sites
+    site_string = ""
+    for s in sites(unitcell)
+        if sitelabeltype <: AbstractString
+            site_string = site_string * "            newSite(Float64" * string(point(s)) * ", LS(\"" * string(label(s)) *  "\"), S),\n"
+        else
+            site_string = site_string * "            newSite(Float64" * string(point(s)) * ", LS(" * string(label(s)) *  "), S),\n"
+        end
+    end
+    if length(site_string) > 2
+        site_string = site_string[1:end-2] * "\n"
+    end
+
+    # build a string for all bonds
+    bond_string = ""
+    for b in bonds(unitcell)
+        if bondlabeltype <: AbstractString #newBond(1,1, LB("1"), (+1,0), B)
+            bond_string = bond_string * "            newBond(" *
+                string(from(b)) * ", " * string(to(b)) *
+                ", LB(\"" * string(label(b)) *  "\"), " * string(wrap(b)) * ", B),\n"
+        else
+            bond_string = bond_string * "            newBond(" *
+                string(from(b)) * ", " * string(to(b)) *
+                ", LB(" * string(label(b)) *  "), " * string(wrap(b)) * ", B),\n"
+        end
+    end
+    if length(bond_string) > 2
+        bond_string = bond_string[1:end-2] * "\n"
+    end
+
     # generate the complete generating code as a string
     generating_code = "function getUnitcell" * uppercase(name[1]) * name[2:end] * "(\n" *
         "            unitcell_type :: Type{U},\n" *
-        "            version :: Val{1}\n" *
+        "            version       :: Val{" * string(version) * "}\n" *
         "        ) :: U where {LS<:" *
         string(sitelabeltype) * ",LB<:" * string(bondlabeltype) *
-        ",S<:AbstractSite{LS," * string(D) * "},B<:AbstractBond{LB," * string(N) *
-        "},U<:AbstractUnitcell{S,B}}"
+        ",S<:AbstractSite{LS," * string(D) * "},B<:AbstractBond{LB," * string(N) * "}, " *
+        "U<:AbstractUnitcell{S,B}}\n" *
+        "    \n" *
+        "    # return a new Unitcell\n" *
+        "    return newUnitcell(\n" *
+        "        # Bravais lattice vectors\n" *
+        "        Vector{Float64}[\n" *
+        lattice_vector_string *
+        "        ],\n" *
+        "        # Sites\n" *
+        "        S[\n" *
+        site_string *
+        "        ],\n" *
+        "        # Bonds\n" *
+        "        B[\n" *
+        bond_string *
+        "        ],\n" *
+        "        # Type of the unitcell\n" *
+        "        U\n" *
+        "    )\n" *
+        "end"
+
     # print the generating code
-    print(generating_code)
-end
-
-function getUnitcellSquare(
-            unitcell_type :: Type{U},
-            version :: Val{1}
-        ) :: U where {LS<:AbstractString,LB<:AbstractString,S<:AbstractSite{LS,2},B<:AbstractBond{LB,2},U<:AbstractUnitcell{S,B}}
-
-    # return a new Unitcell
-    return newUnitcell(
-        # lattice vectors
-        Vector{Float64}[
-            Float64[1, 0],
-            Float64[0, 1]
-        ],
-        # sites
-        S[
-            newSite(Float64[0,0], LS("1"), S)
-        ],
-        # bonds
-        B[
-            newBond(1,1, LB("1"), (+1,0), B),
-            newBond(1,1, LB("1"), (-1,0), B),
-            newBond(1,1, LB("1"), (0,+1), B),
-            newBond(1,1, LB("1"), (0,-1), B)
-        ],
-        # give the unitcell type
-        U
-    )
+    return generating_code
 end
