164e1d75d60073a588ca0be467fecf67c6b77dad
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-06-26 16:59:36 +0200

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using LatticePhysics
 using Test
+using Pkg
 
 @testset "LatticePhysics.jl" begin
-    # Write your own tests here.
+
 end
+
+
+# test all submodules
+Pkg.test("LatPhysBase")
+Pkg.test("LatPhysUnitcellLibrary")
+Pkg.test("LatPhysLatticeConstruction")
+Pkg.test("LatPhysLatticeModification")
+Pkg.test("LatPhysReciprocal")
+Pkg.test("LatPhysBandstructures")
+Pkg.test("LatPhysLuttingerTisza")
